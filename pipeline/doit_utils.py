9235ba6b65152805ef3de2f4205a8aac48a74057
C. Titus Brown <titus@idyll.org>
2016-09-18 09:03:45 -0700

    conversion to a self-hosted pipeline from dodo.py

---
+++ pipeline/doit_utils.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+from doit.cmd_base import TaskLoader
+from doit.doit_cmd import DoitMain
+from doit.task import dict_to_task
+
+
+_task_count = 1
+def make_task(task_dict_func):
+    '''Wrapper to decorate functions returning pydoit
+    `Task` dictionaries and have them return pydoit `Task`
+    objects
+    '''
+    def d_to_t(*args, **kwargs):
+        global _task_count
+        name = "{0}.{1}".format(str(_task_count), str(task_dict_func))
+        _task_count += 1
+    
+        ret_dict = task_dict_func(*args, **kwargs)
+        ret_dict['name'] = name
+        return dict_to_task(ret_dict)
+    return d_to_t
+
+
+def run_tasks(tasks, args, config={'verbosity': 0}):
+    '''Given a list of `Task` objects, a list of arguments,
+    and a config dictionary, execute the tasks.
+    '''
+
+    if type(tasks) is not list:
+        raise TypeError('tasks must be of type list.')
+
+    class Loader(TaskLoader):
+        @staticmethod
+        def load_tasks(cmd, opt_values, pos_args):
+            return tasks, config
+
+    DoitMain(Loader()).run(args)
