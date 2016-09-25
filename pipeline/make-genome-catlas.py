34a72269e564d8bfbc92ba8fbbd9b2dfff406c76
C. Titus Brown <titus@idyll.org>
2016-09-25 07:41:04 -0700

    add initial 15genome

--- pipeline/make-genome-catlas.py
+++ pipeline/make-genome-catlas.py
@@ -19,18 +19,14 @@ def main():
     parser.add_argument('dirname')
     parser.add_argument('inp_fasta', nargs='+')
     parser.add_argument('-r', '--radius', type=int, default=3)
+    parser.add_argument('-M', '--memory', type=float, default=1e9)
 
     parser.add_argument('--clean', default=False, action='store_true')
     args = parser.parse_args()
 
-    try:
-        os.mkdir('temp')
-    except OSError:
-        pass
-    #set_tempdir('./temp/')
-
     tasks = []
-    tasks.append(task_walk_dbg(args.inp_fasta, args.dirname, label=True))
+    tasks.append(task_walk_dbg(args.inp_fasta, args.dirname,
+                               memory=args.memory, label=True))
     tasks.append(task_build_catlas(args.dirname, args.radius))
 
     if args.clean:
